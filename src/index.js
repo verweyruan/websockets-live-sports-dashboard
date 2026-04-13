c3392279ba6301db3f9261b3efeb46300767f197
Ruan Verwey <ruanv0802@icloud.com>
2026-04-13 17:17:54 +0200

    implement pub sub broadcasting for commentaries

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import http from 'http';
 import { matchRouter } from './routes/matches.js';
 import { attachWebSocketServer } from './ws/server.js';
 import { securityMiddleware } from './arcjet.js';
+import { commentaryRouter } from './routes/commentary.js';
 
 
 const PORT = Number(process.env.PORT || 8000);
@@ -20,10 +21,12 @@ app.get('/', (req, res) => {
 
 app.use(securityMiddleware());
 
-app.use('/matches', matchRouter)
+app.use('/matches', matchRouter);
+app.use('/matches/:id', commentaryRouter);
 
-const { broadcastMatchCreated } = attachWebSocketServer(server);
+const { broadcastMatchCreated, broadcastCommentary } = attachWebSocketServer(server);
 app.locals.broadcastMatchCreated = broadcastMatchCreated;
+app.locals.broadcastCommentary = broadcastCommentary;
 
 server.listen(PORT, HOST, () => {
     const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
